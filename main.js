eb4e1ceb80eeb00236d55d52db201b4c9eaab593
JeanMichelz007 <jmsf061207@gmail.com>
2026-09-21 21:46:33 -0300

    atualização javascript

--- main.js
+++ main.js
@@ -28,3 +28,17 @@ function somar (x,y){
 }
 
 FIM MODELO 3*/
+
+const f = document.getElementById("formulario")
+
+f.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const v1 = Number(document.getElementById("num1").value)
+    const v2 = Number(document.getElementById("num2").value)
+
+    const soma = v1+v2
+
+    document.getElementById("resultado").textContent=soma
+
+})
